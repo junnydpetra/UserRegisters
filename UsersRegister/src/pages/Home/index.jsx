15e73c5f2627a945071cff644f0ac040799a2aee
junnydpetra <junnycmenezes@gmail.com>
2024-12-19 01:42:01 -0300

    upload commit

---
+++ UsersRegister/src/pages/Home/index.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState, useRef } from 'react'
+import './style.css'
+import Trash from '../../assets/trash.svg'
+import api from '../../services/api'
+
+function Home() {
+  const [users, setUsers] = useState([])
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+  async function getUsers() {
+    const usersFromApi = await api.get('/users')
+
+    setUsers(usersFromApi.data)
+  }
+
+  async function createUsers() {
+    await api.post('/users', {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+      email: inputEmail.current.value
+    })
+
+    getUsers()
+  }
+
+  async function deleteUsers(id) {
+    await api.delete(`/users/${id}`)
+    getUsers()
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  return (
+    
+      <div className='container'>
+        <form>
+          <h1>Registro de Usuários</h1>
+          <input name='name' type='text' placeholder='Nome' ref={inputName}/>
+          <input name='age' type='number' placeholder='Idade' ref={inputAge}/>
+          <input name='email' type='email' placeholder='Email' ref={inputEmail}/>
+          <button type='button' onClick={createUsers}>Enviar</button>
+        </form>
+
+        { users.map( user => (
+          
+            <div key={user.id} className='card'>
+              <div>
+                <p>Nome: <span>{user.name}</span></p>
+                <p>Idage: <span>{user.age}</span></p>
+                <p>Email: <span>{user.email}</span></p>
+              </div>
+              <button onClick={() => deleteUsers(user.id)}>
+                <img src={Trash} alt="excluir usuário" />
+              </button>
+            </div>
+
+        ) ) }
+
+      </div>
+  )
+}
+
+export default Home
